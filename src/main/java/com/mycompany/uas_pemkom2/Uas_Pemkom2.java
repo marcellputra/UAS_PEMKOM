a6dee5d5c0d9435e1d5a7cb1d58ab8ddc584aa96
Marcell Putra <marcellputra0704@gmail.com>
2025-06-16 11:53:36 +0700

    UAS_Pemkom2

---
+++ src/main/java/com/mycompany/uas_pemkom2/Uas_Pemkom2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uas_pemkom2;
+
+/**
+ *
+ * @author USER
+ */
+public class Uas_Pemkom2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
